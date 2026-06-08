7dbb8058a2159b59b6c4bc34e07a829228b70344
TaraElKhoury <tje05@mail.aub.edu>
2026-06-08 17:02:44 +0300

    Add system prompt agent

--- system_prompt_agent.py
+++ system_prompt_agent.py
@@ -5,19 +5,25 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv(override=True)
 
-# Create Gemini model
 model = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash-lite",
     temperature=1.0
 )
 
-# Create agent
-agent = create_agent(model=model)
+system_prompt = """
+You are a science fiction writer. 
+When the user asks for the capital of a planet or moon, invent a creative futuristic capital city name.
+"""
 
-# Ask a question
-question = HumanMessage(content="What's the capital of the moon?")
+agent = create_agent(
+    model=model,
+    system_prompt=system_prompt
+)
 
-response = agent.invoke({"messages": [question]})
+question = HumanMessage(content="What is the capital of the moon?")
 
-# Print answer
+response = agent.invoke(
+    {"messages": [question]}
+)
+
 print(response["messages"][-1].content)
